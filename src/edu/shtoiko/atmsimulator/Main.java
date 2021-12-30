82411c069cb148ada52180f56242c27404369672
Vasyl <mr.shtoiko@gmail.com>
2021-12-30 17:24:03 +0200

    Change imports in Main.java#3

--- src/edu/shtoiko/atmsimulator/Main.java
+++ src/edu/shtoiko/atmsimulator/Main.java
@@ -1,20 +1,23 @@
 package edu.shtoiko.atmsimulator;
 
-import edu.shtoiko.atmsimulator.atmresource.*;
+import edu.shtoiko.atmsimulator.atmresource.ATMloading;
+import edu.shtoiko.atmsimulator.atmresource.DBsimulator;
+import edu.shtoiko.atmsimulator.atmresource.Withdrafting;
+
 
 public class Main {
   public static void main(String[] args) {
     int[] load = {2500, 0, 0, 1, 0}; // enter several banknotes to load (fifty, hundred, two hundred, five hundred, thousand)
     ATMloading loading = new ATMloading(); // first load
-    System.out.println("Load is " + loading.ATMload(load));
+    System.out.println("Load is " + loading.ATMload(load)); //       loading.ATMload(load);
     int[] load2 = {2500, 0, 0, 0, 0}; // enter several banknotes to load (fifty, hundred, two hundred, five hundred, thousand)
     ATMloading loading2 = new ATMloading(); // second load
     System.out.println("Load is " + loading2.ATMload(load2));
     System.out.println("Before output :");
     DBsimulator.writeResource();
-    Withdrafting vivod = new Withdrafting(2950); // sum to withdraw
+    Withdrafting withdraw = new Withdrafting(2950); // sum to withdraw
     int[] outputBanknotes;
-    outputBanknotes = vivod.getOutputingBanknotes();
+    outputBanknotes = withdraw.getOutputingBanknotes();
     for (int banknotes : outputBanknotes) System.out.println(banknotes);
     System.out.println("After output :");
     DBsimulator.writeResource();
