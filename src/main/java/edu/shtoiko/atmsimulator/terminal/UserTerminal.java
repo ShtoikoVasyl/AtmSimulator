7d46cd073c7aee81758f31c24329da14889da77d
Vasyl <mr.shtoiko@gmail.com>
2022-01-27 18:08:05 +0200

    Rewrite UserTerminal.java #7

--- src/main/java/edu/shtoiko/atmsimulator/terminal/UserTerminal.java
+++ src/main/java/edu/shtoiko/atmsimulator/terminal/UserTerminal.java
@@ -1,34 +1,51 @@
 package edu.shtoiko.atmsimulator.terminal;
 
+import edu.shtoiko.atmsimulator.atmresource.DBsimulator;
 import edu.shtoiko.atmsimulator.atmresource.Withdrafting;
-import edu.shtoiko.atmsimulator.atmresource.ATMloading;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class UserTerminal {
+
+public class UserTerminal extends JFrame{
     public UserTerminal(){
 
-        int[] load = {25, 20, 1, 10, 20}; // enter several banknotes to load (fifty, hundred, two hundred, five hundred, thousand)
-        ATMloading loading = new ATMloading(); // first load
-        loading.ATMload(load);
+//        int[] load = {25, 20, 1, 10, 20}; // enter several banknotes to load (fifty, hundred, two hundred, five hundred, thousand)
+//        ATMloading loading = new ATMloading(); // first load
+//        loading.ATMload(load);
 
-        JFrame userFrame = new JFrame();
-        userFrame.setVisible(true);
-        userFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        userFrame.setBounds(300,100,600,600);
+        TerminalFrame userFrame = new TerminalFrame();
 
-        String message = "What amount do you want to get?";
-        JLabel text = new JLabel();
+
+//        Header header = new Header();
+
+        JLabel lastMessage = new JLabel();
+        lastMessage.setText("Welcome, you authorized as 'USER_NAME'.");
+        lastMessage.setHorizontalAlignment(JLabel.CENTER);
+        lastMessage.setBounds(20,5, 435,35);
+        lastMessage.setFont(new Font("", Font.ITALIC, 14));
+
+
+        String message = ("<html><div style= 'text-align: center;'>What amount do you want to get?<br>Available banknotes: " + DBsimulator.getAvailableBanknotes() + "</div></html>");
+        JLabel text = new JLabel("", SwingConstants.CENTER);
         text.setText(message);
+        text.setFont(new Font("",Font.BOLD,15));
+        text.setBounds(80,45,315,100);
+
 
         JTextField inputSum = new JTextField(5);
+        inputSum.setBounds(130,135,215,50);
+        inputSum.setFont(new Font("", Font.BOLD, 19));
+        inputSum.setHorizontalAlignment(JTextField.CENTER);
 //        String input = inputSum.getText();
 //        int sum = Integer.parseInt(input);
 
         JButton get = new JButton();
-        get.setText("Get it...");
+        get.setText("Get money");
+        get.setFont(new Font("",Font.BOLD,17));
+        get.setBackground(new Color(138,200,168));
+        get.setBounds(160,235,155,50);
         get.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -36,6 +53,7 @@ public class UserTerminal {
                 int sum = Integer.parseInt(input);
                 Withdrafting withdrafting = new Withdrafting(sum);
                 int[] withdraftBanknotes = withdrafting.getOutputingBanknotes();
+                String messageUpdate = "Withdraft complete";
 
 
                 JOptionPane.showMessageDialog(new JPanel(), "withdraft: \n" +
@@ -44,22 +62,39 @@ public class UserTerminal {
                         "two hundred - " + withdraftBanknotes[2] + "\n" +
                         "five hundred - " + withdraftBanknotes[3] + "\n" +
                         "thousand - " + withdraftBanknotes[4] + "\n");
+                String message = ("<html><div style= 'text-align: center;'>What amount do you want to get?<br>Available banknotes: " + DBsimulator.getAvailableBanknotes() + "</div></html>");
+
+
+                        text.setText(message);
             }
         });
 
+        JPanel withdrawPanel = new JPanel();
+        withdrawPanel.setBackground(new Color(240,240,240));
+        withdrawPanel.setBounds(70,0,475,350);
+        withdrawPanel.setLayout(null);
+        withdrawPanel.add(text);
+        withdrawPanel.add(inputSum);
+        withdrawPanel.add(get);
+        withdrawPanel.add(lastMessage);
 
 
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.add(text, BorderLayout.NORTH);
-        mainPanel.add(inputSum, BorderLayout.CENTER);
-        mainPanel.add(get, BorderLayout.SOUTH);
+
+//        JPanel mainPanel = new JPanel();
+        userFrame.mainPanel.setBackground(new Color(250,255,244));
+        userFrame.mainPanel.setLayout(null);
+//        mainPanel.add(text);
+//        mainPanel.add(inputSum);
+//        mainPanel.add(get);
+//        mainPanel.add(header);
+   //     mainPanel.add(lastMessage);
+        userFrame.mainPanel.add(withdrawPanel);
 
 
 
 
 
-        userFrame.getContentPane().add(mainPanel);
+//        userFrame.getContentPane().add(mainPanel);
 
 
 
