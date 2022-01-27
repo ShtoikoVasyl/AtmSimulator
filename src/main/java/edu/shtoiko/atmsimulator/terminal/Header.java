744ced4e7f75ccd9d8fcb1988e9e123ab7c15712
Vasyl <mr.shtoiko@gmail.com>
2022-01-27 17:33:43 +0200

    Add Header.java, top area of window #7

---
+++ src/main/java/edu/shtoiko/atmsimulator/terminal/Header.java
@@ -0,0 +1,18 @@
+package edu.shtoiko.atmsimulator.terminal;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Header extends JPanel{
+
+
+    protected Header() {
+        this.setBounds(0, 0, 615, 125);
+        this.setBackground(Color.WHITE);
+        this.setLayout(null);
+        JLabel logo = new JLabel(new ImageIcon("src/bankLogo.jpg"));
+        logo.setBounds(35, 15, 300, 100);
+        this.add(logo);
+    }
+
+}
