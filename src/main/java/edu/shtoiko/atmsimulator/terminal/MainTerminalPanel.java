2ca1d95f65c467f3da5c86a7dd2b95aafdbf1509
Vasyl <mr.shtoiko@gmail.com>
2022-01-27 17:35:43 +0200

    Add MainTerminalPanel.java, main area of window #7

---
+++ src/main/java/edu/shtoiko/atmsimulator/terminal/MainTerminalPanel.java
@@ -0,0 +1,12 @@
+package edu.shtoiko.atmsimulator.terminal;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MainTerminalPanel extends JPanel {
+    protected MainTerminalPanel(){
+        this.setBounds(0,125,615,475);
+    }
+
+}
