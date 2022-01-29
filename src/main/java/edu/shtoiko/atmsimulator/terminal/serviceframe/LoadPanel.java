4a64f1e3ed3a3b00c1bb3b8b855d1229edbf28a7
Vasyl <mr.shtoiko@gmail.com>
2022-01-29 19:51:05 +0200

    Add LoadPanel #7

--- src/main/java/edu/shtoiko/atmsimulator/terminal/serviceframe/LoadPanel.java
+++ src/main/java/edu/shtoiko/atmsimulator/terminal/serviceframe/LoadPanel.java
@@ -1,32 +1,61 @@
 package edu.shtoiko.atmsimulator.terminal.serviceframe;
 
+import edu.shtoiko.atmsimulator.datawarehouse.DBsimulator;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class LoadPanel extends JPanel {
 
-    public InputPanel fifty;
-    public InputPanel hundred;
-    public InputPanel twoHundred;
-    public InputPanel fiveHundred;
-    public InputPanel thousand;
+    protected InputPanel fifty;
+    protected InputPanel hundred;
+    protected InputPanel twoHundred;
+    protected InputPanel fiveHundred;
+    protected InputPanel thousand;
+    protected JLabel loadToMaxLeft;
+
+    final int maxTotalBanknotes = 5000;
 
     public LoadPanel(){
-        System.out.println("text");
+
         this.setLayout(null);
 
         fifty = new InputPanel(20, "Load banknotes 50");
         this.add(fifty);
+        hundred = new InputPanel(60, "Load banknotes 100");
+        this.add(hundred);
+        twoHundred = new InputPanel(100, "Load banknotes 200");
+        this.add(twoHundred);
+        fiveHundred = new InputPanel(140, "Load banknotes 500");
+        this.add(fiveHundred);
+        thousand = new InputPanel(180, "Load banknotes 1000");
+        this.add(thousand);
 
+
+
+        this.loadToMaxLeft = new JLabel();
+        loadToMaxLeft.setText(Integer.toString(maxTotalBanknotes - DBsimulator.getTotalBanknotes()));
+        loadToMaxLeft.setFont(new Font("", Font.BOLD, 15));
+        loadToMaxLeft.setBounds(15, 230, 130, 30);
+        JLabel loadToMaxRight = new JLabel();
+        loadToMaxRight.setText("can be loaded");
+        loadToMaxRight.setHorizontalAlignment(JLabel.RIGHT);
+        loadToMaxRight.setFont(new Font("", Font.BOLD, 15));
+        loadToMaxRight.setBounds(145, 230, 210, 30);
+        this.add(loadToMaxLeft);
+        this.add(loadToMaxRight);
     }
 
-@Override
-    public void setBackground(Color color){
+    public void setBackgroundColor(Color color){
         super.setBackground(color);
-//        if (fifty == null){
-//            return;
-//        }
-            fifty.setBackground(color);
+        fifty.setBackground(color);
+        hundred.setBackground(color);
+        twoHundred.setBackground(color);
+        fiveHundred.setBackground(color);
+        thousand.setBackground(color);
     }
 
+    public int getMaxTotalBanknotes() {
+        return maxTotalBanknotes;
+    }
 }
